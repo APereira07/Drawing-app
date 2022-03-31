fde0df0067ca19010b3d025ed6c5c425903e9129
Andres Pereira <a07pereira@gmail.com>
2022-03-30 22:23:51 -0400

    refactor canvas and fill in color

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,22 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 30;
+let x = 50;
+let y = 50;
 
 canvas.addEventListener('mousedown', () => {});
 
 function drawCricle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
-    ctx.stroke();
+    ctx.fill();
 }
 
-drawCricle(200, 200);
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCricle(50, 50);
+
+    requestAnimationFrame(draw);
+}
+
+draw();
