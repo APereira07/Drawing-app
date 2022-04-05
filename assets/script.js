0c5733aa96bd907fc8f771f6868e7137708b904e
Andres Pereira <a07pereira@gmail.com>
2022-04-05 17:19:29 -0400

    add size feature and toolbox

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,87 @@
-const canvas = document.getElementById('canvas');
-const increaseBtn = document.getElementById('increase');
-const decreaseBtn = document.getElementById('decrease');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
+const ctx = canvas.getContext("2d");
 
-let size = 20;
+let size = 30;
 let isPressed = false;
+let color = "black";
+let x = undefined;
+let y = undefined;
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener("mousedown", (e) => {
     isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
 });
 
-canvas.addEventListener('mouseup', () => {
+canvas.addEventListener("mouseup", (e) => {
     isPressed = false;
+
+    x = undefined;
+    y = undefined;
 });
 
-canvas.addEventListener('mousemove', (e) => {
-    if(isPressed) {
-        const x = e.offsetX;
-        const y = e.offsetY;
+canvas.addEventListener("mousemove", (e) => {
+    if (isPressed) {
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
 
-        drawCricle(x, y);
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+        x = x2;
+        y = y2;
     }
-
 });
 
-function drawCricle(x, y) {
+function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
     ctx.fill();
 }
 
-increaseBtn.addEventListener('click', () =>{
-    size += 5;
-
-    if(size > 50) {
-        size = 50;
-    }
-})
-
-decreaseBtn.addEventListener('click', () =>{
-    size -= 5;
-
-    if(size < 5) {
-        size = 5;
-    }
-})
-
-/*function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCricle(x++, y);
-
-    requestAnimationFrame(draw);
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
 }
 
-draw(); */
+increaseBtn.addEventListener("click", () => {
+    size += 5;
+
+    if (size > 50) {
+        size = 50;
+    }
+
+    updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+    size -= 5;
+
+    if (size < 5) {
+        size = 5;
+    }
+
+    updateSizeOnScreen();
+});
+
+colorEl.addEventListener("change", (e) => {
+    color = e.target.value;
+});
+
+clearEl.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+function updateSizeOnScreen() {
+    sizeEl.innerText = size;
+}
